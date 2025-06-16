14a5a0c21c23b4ce65f001641276e356a1fd4db3
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-16 15:08:59 +0530

    vectore store code created

---
+++ app/load_documents.py
@@ -0,0 +1,17 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+loader=DirectoryLoader("resources:",glob="**/*.txt")
+documents=loader.load()
+
+splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+chunks=splitter.split_documents(documents)
+
+embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+db=Chroma.from_documents(documents=chunks,embedding=embedding,persist_directory="./chroma_db")
+
+db.persist()
+print("vectorstore created and persisted successfully.")
