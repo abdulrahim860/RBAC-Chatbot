e687ab3211bc6756cc136a8449f34b99ad21daee
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-16 14:45:50 +0530

    rag code updated

---
+++ app/utils/rag.py
@@ -0,0 +1,13 @@
+from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+llm=Ollama(modle='llama3')
+embedding=HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
+vectorstore=Chroma(persitant_directory="./chroma_db",embedding_function=embedding)
+
+def get_response(query:str,role:str):
+    retriever=vectorstore.as_retreiver(search_kwargs={"k":3,"filter":{"role":role}})
+    chain=RetrievalQA.from_chain_type(llm=llm,retriever=retriever)
+    return chain.run(query)
