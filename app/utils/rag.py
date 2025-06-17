23eff48834f2183d474b986abd657a4ce5d5b660
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-17 17:05:13 +0530

    error solved

--- app/utils/rag.py
+++ app/utils/rag.py
@@ -8,6 +8,6 @@ embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-
 vectorstore=Chroma(persist_directory="./chroma_db",embedding_function=embedding)
 
 def get_response(query:str,role:str):
-    retriever=vectorstore.as_retreiver(search_kwargs={"k":3,"filter":{"role":role}})
+    retriever=vectorstore.as_retriever(search_kwargs={"k":3,"filter":{"role":role}})
     chain=RetrievalQA.from_chain_type(llm=llm,retriever=retriever)
     return chain.run(query)
