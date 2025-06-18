95d55ef3e6a6b57b7d6938bfd1af4b740f09d8c8
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-18 17:55:18 +0530

    error solved

--- app/utils/rag.py
+++ app/utils/rag.py
@@ -10,4 +10,4 @@ vectorstore=Chroma(persist_directory="./chroma_db",embedding_function=embedding)
 def get_response(query:str,role:str):
     retriever=vectorstore.as_retriever(search_kwargs={"k":3,"filter":{"role":role}})
     chain=RetrievalQA.from_chain_type(llm=llm,retriever=retriever)
-    return chain.run(query)
+    return chain.invoke({"input":query}) 
