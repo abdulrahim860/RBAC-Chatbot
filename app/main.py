f58018dc51a94c1902662fba6538a3c386e17fc3
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-19 13:52:30 +0530

    cors middleware added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.services.auth import router as auth_router
 from app.services.chat import router as chat_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 app.include_router(chat_router, prefix="/chat")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
