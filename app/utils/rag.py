1ca45d767265338ab989f3dbba3f8fc3335f4fd2
abdulrahim860 <abdulrahim82436@gmail.com>
2025-06-28 19:38:38 +0530

    prompt template added

--- app/utils/rag.py
+++ app/utils/rag.py
@@ -2,12 +2,27 @@ from langchain_ollama import OllamaLLM
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
 llm=OllamaLLM(model='llama3')
 embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 vectorstore=Chroma(persist_directory="./chroma_db",embedding_function=embedding)
 
+prompt=PromptTemplate(
+    input_variables=["context","question"],
+    template=(
+        "use ONLY the following context to answer the question."
+        "Do not use prior knowledge or make up answers.\n\n"
+        "Context:\n{context}\n\nQuestion: {question}\nAnswer:"
+    )
+)
 def get_response(query:str,role:str):
     retriever=vectorstore.as_retriever(search_kwargs={"k":4,"filter":{"role":role}})
-    chain=RetrievalQA.from_chain_type(llm=llm,retriever=retriever)
-    return chain.invoke({"query":query}) 
+    chain=RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=retriever,
+        return_source_documents=False,
+        chain_type_kwargs={"prompt":prompt}
+    )
+    response = chain.invoke({"query": query})
+    return response['result'].replace('\n', ' ').strip() 
